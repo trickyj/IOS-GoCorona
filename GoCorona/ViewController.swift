9c723e4f8271641385197159496e0f9c83473cff
trickyj <pop.tricky@gmail.com>
2021-06-12 08:07:24 +0530

    Initial Commit

---
+++ GoCorona/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoCorona
+//
+//  Created by starman on 12/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
